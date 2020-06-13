e90ce6e266d2c6189f8e10bc8569b7a2f7cf5a73
surajelijah <suraj.elijah@gmail.com>
2020-06-13 17:25:57 -0400

    Added ip_math.c and relevant functions

---
+++ ip_math.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+void get_broadcast_address(char*ip_addr,char mask,char* output_buffer){
+
+}
+
+
+unsigned int get_ip_integral_equivalent(char* ip_address){
+
+
+	return 0;
+}
+
+void get_abcd_ip_format(unsigned int ip_address,char* output_buffer){
+
+
+}
+
+void get_network_id(char* ip_address,char msk,char* output_buffer){
+
+
+}
+
+
+unsigned int get_subnet_cardinality(char mask){
+	
+	return 0;
+}
+
+int check_ip_subnet_membership(char* network_id,char mask,char* check_ip){
+
+	return 0;
+}
+
+
+
+int main()
+{
+	
+	return 0;
+}
+
+
